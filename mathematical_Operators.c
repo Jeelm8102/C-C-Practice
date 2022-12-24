a05ed29088b9924924832e65c691e83c565f9bd2
Jeel <jeelmaheshwari812@gmail.com>
2022-12-24 18:59:47 +0530

    basic implementation of mathematical operators for various operations

---
+++ mathematical_Operators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+int main()
+{
+    float a=21, b=36, c, d, e;
+    float f;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    printf("For a and b \n\n\nadd=%.2f\nsub=%.2f \nmulti=%.2f \ndiv=%.2f,",c,d,e);
+    getch();
+    return 0;
+}
