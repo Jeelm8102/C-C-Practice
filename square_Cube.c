afc56372eaf8f03231840b3af993e005e4eaa8ce
Jeel <jeelmaheshwari812@gmail.com>
2022-12-29 20:03:29 +0530

    square and cube of a given number

---
+++ square_Cube.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//SQUARE AND CUBE
+int main()
+{
+    int num;
+    printf("Enter the number:\n");
+    scanf("%d",&num);
+
+    printf("The given number is:%d.\n",num);
+    printf("The Square of given number is:%d.\n",num*num);
+    printf("The cube of given number is:%d.\n",num*num*num);
+    return 0;
+
+}
