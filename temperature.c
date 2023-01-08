2c96159abb04ad31ec0862aa009d64abadf53216
Jeel <jeelmaheshwari812@gmail.com>
2023-01-08 20:15:56 +0530

    simple c program to measure temperature

---
+++ temperature.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+//Measure temperature
+
+int main()
+{
+    int a[7][2];
+    Printf("Enter the value of temperature");
+    for(i=0;i<7;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    return 0;
+}
+
